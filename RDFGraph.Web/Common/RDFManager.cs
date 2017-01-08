46745304a5a0d5cb336adaed65b962acafe3c2ae
Konrad <Konrad@DESKTOP-3MCCUK0>
2017-01-08 23:26:47 +0100

    Fixed model from method ReadData

--- RDFGraph.Web/Common/RDFManager.cs
+++ RDFGraph.Web/Common/RDFManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RDFGraph.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,11 +10,16 @@ namespace RDFGraph.Web.Common
 {
     public static class RDFManager
     {
-        public static IEnumerable<Triple> GetTriples(Uri uri)
+        public static IEnumerable<TripleViewModel> GetTriples(Uri uri)
         {
             IGraph graph = new Graph();
             UriLoader.Load(graph, new Uri("http://dbpedia.org/resource/Barack_Obama"));
-            return graph.Triples;
+            return graph.Triples.Select(n => new TripleViewModel()
+            {
+                Subject = n.Subject,
+                Predicate = n.Predicate,
+                Object = n.Object
+            });
         }
     }
 }
---
+++ RDFGraph.Web/Models/TripleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VDS.RDF;
+
+namespace RDFGraph.Web.Models
+{
+    public class TripleViewModel
+    {
+        public INode Subject { get; set; }
+        public INode Predicate { get; set; }
+        public INode Object { get; set; }
+
+    }
+}

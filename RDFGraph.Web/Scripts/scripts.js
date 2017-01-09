c520be206a97c2ce60c7e9c773de516c98a3f7a2
Dawid Chróścielski <deejaylysy@gmail.com>
2017-01-09 15:39:40 +0100

    Added viewer libs

--- RDFGraph.Web/Scripts/scripts.js
+++ RDFGraph.Web/Scripts/scripts.js
@@ -10,13 +10,85 @@
             url: uri
         },
         success: function (data) {
-            //DATA IS OUR RDF TRIPLE MODELS
-            //USE IT TO GENERATE RDF GRAPH
-
+            console.log(data);
         },
         error: function () {
             toastr.error("Podano niewłaściwy adres URI");
         }
+    });
+
+}
+
+function test() {
+    console.log("ASD");
+    var testing = window.testing = cytoscape({
+        container: document.getElementById('testing'),
+
+        boxSelectionEnabled: false,
+        autounselectify: true,
+
+        layout: null,
+
+        style: [
+            {
+                selector: 'node',
+                style: {
+                    'content': 'data(id)',
+                    'text-opacity': 0.5,
+                    'text-valign': 'center',
+                    'text-halign': 'right',
+                    'background-color': '#11479e'
+                }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                    'width': 4,
+                    'target-arrow-shape': 'triangle',
+                    'line-color': '#9dbaea',
+                    'target-arrow-color': '#9dbaea',
+                    'curve-style': 'bezier'
+                }
+            }
+        ],
+
+        elements: {
+            nodes: [
+                { data: { id: 'n0' } },
+                { data: { id: 'n1' } },
+                { data: { id: 'n2' } },
+                { data: { id: 'n3' } },
+                { data: { id: 'n4' } },
+                { data: { id: 'n5' } },
+                { data: { id: 'n6' } },
+                { data: { id: 'n7' } },
+                { data: { id: 'n8' } },
+                { data: { id: 'n9' } },
+                { data: { id: 'n10' } },
+                { data: { id: 'n11' } },
+                { data: { id: 'n12' } },
+                { data: { id: 'n13' } },
+                { data: { id: 'n14' } },
+                { data: { id: 'n15' } },
+                { data: { id: 'n16' } }
+            ],
+            edges: [
+                { data: { source: 'n0', target: 'n1' } },
+                { data: { source: 'n1', target: 'n2' } },
+                { data: { source: 'n1', target: 'n3' } },
+                { data: { source: 'n4', target: 'n5' } },
+                { data: { source: 'n4', target: 'n6' } },
+                { data: { source: 'n6', target: 'n7' } },
+                { data: { source: 'n6', target: 'n8' } },
+                { data: { source: 'n8', target: 'n9' } },
+                { data: { source: 'n8', target: 'n10' } },
+                { data: { source: 'n11', target: 'n12' } },
+                { data: { source: 'n12', target: 'n13' } },
+                { data: { source: 'n13', target: 'n14' } },
+                { data: { source: 'n13', target: 'n15' } },
+            ]
+        },
     });
 
 }
